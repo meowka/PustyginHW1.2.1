225ea3be75275241caa71bd42e00ddc1c78dcaff
meowka <Pustygin.a@mail.ru>
2020-03-22 19:53:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hw121/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hw121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOk = (Button) findViewById(R.id.btnOK);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
+        final TextView alertText = (TextView) findViewById(R.id.alertText);
+        final EditText userName = (EditText) findViewById(R.id.userName);
+        final EditText userEmail = (EditText) findViewById(R.id.userEmail);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                alertText.setText("Подписка на рассылку успешно оформлена для пользователя " + userName.getText() + " на электронный адрес " + userEmail.getText());
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName.getText().clear();
+                userEmail.getText().clear();
+                alertText.setText("");
+            }
+        });
+    }
+}
